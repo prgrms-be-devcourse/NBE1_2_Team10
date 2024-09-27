3906c4c18b41cd03ee063e889f67e57a75e43c9e
hgh1472 <hgh1472@naver.com>
2024-09-27 15:37:18 +0900

    Refactor : 영화 장르 및 검색 조건 enum 타입으로 변경

---
+++ application/src/main/java/core/application/movies/constant/Genre.java
@@ -0,0 +1,17 @@
+package core.application.movies.constant;
+
+public enum Genre {
+	ACTION("액션"),
+	SF("SF"),
+	ROMANCE("로맨스"),
+	ANIMATION("애니메이션"),
+	DRAMA("드라마"),
+	THRILLER("스릴러"),
+	COMEDY("코메디");
+
+	public final String PARAMETER;
+
+	Genre(String parameter) {
+		this.PARAMETER = parameter;
+	}
+}
