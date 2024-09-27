0eb8a03230374efe4a473270663b5b48d2137fde
hgh1472 <hgh1472@naver.com>
2024-09-27 16:46:53 +0900

    Feat : 메인페이지 영화 DTO 내용 추가

--- application/src/main/java/core/application/movies/models/dto/MainPageMovieRespDTO.java
+++ application/src/main/java/core/application/movies/models/dto/MainPageMovieRespDTO.java
@@ -1,4 +1,12 @@
 package core.application.movies.models.dto;
 
 public class MainPageMovieRespDTO {
+
+	private String movieId;
+
+	private String title;
+
+	private String posterUrl;
+
+	private String releaseDate;
 }
