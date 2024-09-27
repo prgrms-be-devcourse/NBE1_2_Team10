74317e660bc85a168de47d50e3ae32cd5eeb6930
jbw9964 <jbw9964@gmail.com>
2024-09-27 12:44:45 +0900

    feat: `reviews` 정렬 관련 `enum` 상수 추가

---
+++ application/src/main/java/core/application/reviews/services/ReviewCommentSortOrder.java
@@ -0,0 +1,8 @@
+package core.application.reviews.services;
+
+/**
+ * 리뷰 포스팅 댓글 정렬용 상수
+ */
+public enum ReviewCommentSortOrder {
+    LATEST, LIKE
+}
---
+++ application/src/main/java/core/application/reviews/services/ReviewSortOrder.java
@@ -0,0 +1,8 @@
+package core.application.reviews.services;
+
+/**
+ * 리뷰 포스팅 정렬용 상수
+ */
+public enum ReviewSortOrder {
+    LATEST, LIKE
+}
