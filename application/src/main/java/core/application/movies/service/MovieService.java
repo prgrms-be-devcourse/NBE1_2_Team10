747c9c1cf256a2bdd3162c7188a0d3adc8d6cbe2
shmin <gortmtjdgns0943@gmail.com>
2024-09-29 16:13:39 +0900

    kmdb 기본 셋팅 추가 및 변경
    [추가 사항]
    1. kmdb key, url 추가
    2. default.image 임시 경로 지정
    3. json, WebFlux 설정 라이브러리 추가
    4. MovieDetailRespDTO에 @Data 어노테이션 적용
    5. MovieContoller, MovieServiceImpl 추가
    
    [변경 사항]
    1. MovieService 인터페이스에서 getMainPageMovieInfo, searchMovies 메소드 제거

--- application/src/main/java/core/application/movies/service/MovieService.java
+++ application/src/main/java/core/application/movies/service/MovieService.java
@@ -11,24 +11,6 @@ import core.application.movies.models.dto.MovieSimpleRespDTO;
 public interface MovieService {
 
 	/**
-	 * 메인 페이지에서 찜 많은 순 영화 10개, 평점 높은 순 영화 10개, 최신순 영화 10개의 정보를 보여준다.
-	 * @return 찜 많은 순, 평점 높은 순, 최신순으로 정렬된 영화 정보
-	 */
-	public MainPageMoviesRespDTO getMainPageMovieInfo();
-
-	/**
-	 * 사용자는 장르, 검색어, 정렬 조건을 이용할 수 있다. <br>
-	 * 정렬 조건과 검색어는 필수이지만, 장르는 선택적으로 사용한다.
-	 *
-	 * @param page 검색 페이지 목록
-	 * @param sort 정렬 조건
-	 * @param query 검색어
-	 * @param genre 영화 장르
-	 * @return 검색한 영화 정보
-	 */
-	public List<MovieSimpleRespDTO> searchMovies(Integer page, MovieSearch sort, String query, Genre genre);
-
-	/**
 	 * 사용자가 영화 상세 페이지에서 필요로 하는 정보를 보내준다.
 	 * @param movieId KMDB API의 DOCID
 	 * @return 영화 상세 페이지에서 필요로하는 영화 정보
---
+++ application/src/main/java/core/application/movies/service/MovieServiceImpl.java
@@ -0,0 +1,106 @@
+package core.application.movies.service;
+
+import core.application.movies.models.dto.MovieDetailRespDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class MovieServiceImpl implements MovieService {
+
+	@Value("${kmdb.api.key}")
+	private String apiKey;
+
+	@Value("${kmdb.api.default.image}")
+	private String defaultImgUrl;
+
+	private final WebClient webClient;
+
+
+	public MovieDetailRespDTO getMovieDetailInfo(String movieId) {
+
+		String MovieId = movieId.substring(0, 1);
+		String MovieSeq = movieId.substring(1);
+
+		String response = webClient.get()
+				.uri(uriBuilder -> uriBuilder
+						.path("/search_json2.jsp")
+						.queryParam("ServiceKey", apiKey)
+						.queryParam("detail", "Y")
+						.queryParam("collection", "kmdb_new2")
+						.queryParam("ratedYn", "Y")
+						.queryParam("movieId", MovieId)
+						.queryParam("movieSeq", MovieSeq)
+						.queryParam("listCount", 5)
+						.build())
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+
+		return parseMovieDetail(new JSONObject(response));
+	}
+
+	private MovieDetailRespDTO parseMovieDetail(JSONObject jsonResponse) {
+
+		MovieDetailRespDTO movieDetailRespDTO = new MovieDetailRespDTO();
+
+		JSONObject resultObject = jsonResponse.getJSONArray("Data")
+			.getJSONObject(0)
+			.getJSONArray("Result")
+			.getJSONObject(0);
+
+		// 제목에서 !HS, !HE 제거 후 제목 저장
+		String title = resultObject.optString("title", "").replaceAll("!HS", "").replaceAll("!HE", "").trim();
+		movieDetailRespDTO.setTitle(title);
+
+		// 포스트 링크 저장
+		String imgUrl = resultObject.optString("posters", defaultImgUrl);
+		if (!imgUrl.equals(defaultImgUrl)) {
+			movieDetailRespDTO.setPosterUrl(imgUrl.split("\\|")[0]);
+		} else {
+			movieDetailRespDTO.setPosterUrl(imgUrl);
+		}
+
+		// 장르 저장
+		movieDetailRespDTO.setGenre(resultObject.optString("genre", ""));
+
+		// 대표 개봉일
+		movieDetailRespDTO.setReleaseDate(resultObject.optString("repRlsDate", ""));
+
+		// 줄거리 설정
+		JSONArray plotArray = resultObject.optJSONObject("plots").optJSONArray("plot");
+		for (int j = 0; j < plotArray.length(); j++) {
+			JSONObject plot = plotArray.getJSONObject(j);
+			if (plot.optString("plotLang").equals("한국어")) {
+				movieDetailRespDTO.setOverview(plot.optString("plotText"));
+			}
+		}
+
+		movieDetailRespDTO.setRunningTime(resultObject.optString("runtime", "상영시간"));
+
+		// 배우 목록 설정 (최대 3명)
+		JSONArray actorsArray = resultObject.optJSONObject("actors").optJSONArray("actor");
+		List<String> actorsList = new ArrayList<>();
+		if (actorsArray != null) {
+			for (int k = 0; k < Math.min(actorsArray.length(), 3); k++) {
+				String actorName = actorsArray.getJSONObject(k).optString("actorNm", "배우명");
+				actorsList.add(actorName);
+			}
+		}
+		movieDetailRespDTO.setActors(actorsList);
+
+		JSONArray directorsArray = resultObject.optJSONObject("directors").optJSONArray("director");
+		movieDetailRespDTO.setDirector(directorsArray.getJSONObject(0).optString("directorNm", "감독명"));
+
+		return movieDetailRespDTO;
+	}
+}
