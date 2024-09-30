7ee30972c47114a617a2974872e0578679b2cff8
Lunabell03 <sunnism03@gmail.com>
2024-09-30 10:43:10 +0900

    Fix : UserEntity에서 DibList 칼럼 삭제

--- application/src/main/java/core/application/users/models/entities/UserEntity.java
+++ application/src/main/java/core/application/users/models/entities/UserEntity.java
@@ -2,7 +2,6 @@ package core.application.users.models.entities;
 
 import lombok.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -19,9 +18,4 @@ public class UserEntity {
     private String   alias;
     private String   phoneNum;
     private String   userName;
-
-    /**
-     * 해당 유저가 소유한 찜 목록들
-     */
-    private List<DibEntity> dibEntityList;
 }
