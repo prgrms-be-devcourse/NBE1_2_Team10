41e8bbcfde096e74a5b6e8930bfdcb4a79183ed9
[한도윤] <semin98@naver.com>
2024-09-30 15:52:08 +0900

    리뷰 쿼리 작성

---
+++ application/src/main/java/core/application/reviews/repositories/mapper/ReviewMapper.java
@@ -0,0 +1,36 @@
+package core.application.reviews.repositories.mapper;
+
+import core.application.reviews.models.entities.ReviewEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Mapper
+public interface ReviewMapper {
+
+    ReviewEntity saveNewReview(String movieId, UUID userId, ReviewEntity review);
+
+    Optional<ReviewEntity> findByReviewId(Long reviewId);
+
+    List<ReviewEntity> findByMovieId(String movieId);
+
+    List<ReviewEntity> findByMovieIdOnDateDescend(String movieId);
+
+    List<ReviewEntity> findByMovieIdOnLikeDescend(String movieId);
+
+    List<ReviewEntity> findByMovieIdWithoutContent(String movieId);
+
+    List<ReviewEntity> findByMovieIdWithoutContentOnDateDescend(String movieId);
+
+    List<ReviewEntity> findByMovieIdWithoutContentOnLikeDescend(String movieId);
+
+    List<ReviewEntity> findByUserId(UUID userId);
+
+    List<ReviewEntity> selectAll();
+
+    ReviewEntity editReviewInfo(Long reviewId, ReviewEntity reviewEntity);
+
+    void deleteReview(Long reviewId);
+}
