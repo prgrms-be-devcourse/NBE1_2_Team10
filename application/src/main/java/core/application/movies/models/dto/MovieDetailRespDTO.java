96397480fb332ccaa947fadd775c01f4a6a8ed18
shmin <gortmtjdgns0943@gmail.com>
2024-09-30 17:37:41 +0900

    영화 상세 조회 기능 구현

--- application/src/main/java/core/application/movies/models/dto/MovieDetailRespDTO.java
+++ application/src/main/java/core/application/movies/models/dto/MovieDetailRespDTO.java
@@ -1,5 +1,7 @@
 package core.application.movies.models.dto;
 
+import core.application.movies.constant.Genre;
+import core.application.movies.models.entities.CachedMovieEntity;
 import lombok.Data;
 
 import java.util.List;
@@ -20,11 +22,33 @@ public class MovieDetailRespDTO {
 
 	private String runningTime;
 
-	private List<String> actors;
+	private String actors;
 
 	private String director;
 
-	private Integer dibCnt;
+	private Long dibCnt;
 
-	private String rating;
+	private Long reviewCount;
+
+	private Long commentCount;
+
+	private Long rating;
+
+	public MovieDetailRespDTO toDTO(CachedMovieEntity cachedMovieEntity){
+		this.movieId = cachedMovieEntity.getMovieId();
+		this.title = cachedMovieEntity.getTitle();
+		this.posterUrl = cachedMovieEntity.getPosterUrl();
+		this.genre = cachedMovieEntity.getGenre();
+		this.releaseDate = cachedMovieEntity.getReleaseDate();
+		this.overview = cachedMovieEntity.getPlot();
+		this.runningTime = cachedMovieEntity.getRunningTime();
+		this.actors = cachedMovieEntity.getActors();
+		this.director = cachedMovieEntity.getDirector();
+		this.dibCnt = cachedMovieEntity.getDibCount();
+		this.reviewCount = cachedMovieEntity.getReviewCount();
+		this.commentCount = cachedMovieEntity.getCommentCount();
+		this.rating = cachedMovieEntity.getSumOfRating();
+
+		return this;
+	}
 }
