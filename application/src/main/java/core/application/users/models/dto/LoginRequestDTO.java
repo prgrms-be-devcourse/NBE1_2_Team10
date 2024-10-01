df689ccb6ed035bef4ab0412983096e929b43cc7
minjung <minjung0768@gmail.com>
2024-10-01 17:09:20 +0900

    Feat : 구현 회원 관리 기능 (without token)
    로그인
    로그아웃
    회원 가입
    회원 탈퇴
    사용자 정보 변경
    
    Related to: #1, #2, #3, #4

--- application/src/main/java/core/application/users/models/dto/LoginRequestDTO.java
+++ application/src/main/java/core/application/users/models/dto/LoginRequestDTO.java
@@ -3,12 +3,10 @@ package core.application.users.models.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.UUID;
-
 @Getter
 @AllArgsConstructor
 
-public class MessageResponseDTO {
-    private UUID userId;
-    private String message;
+public class LoginRequestDTO {
+    String userEmail;
+    String userPw;
 }
--- application/src/main/java/core/application/users/service/UserService.java
+++ application/src/main/java/core/application/users/service/UserService.java
@@ -1,17 +1,21 @@
 package core.application.users.service;
 
+import core.application.users.models.dto.LoginRequestDTO;
 import core.application.users.models.dto.MessageResponseDTO;
 import core.application.users.models.dto.UserDTO;
 
+import java.util.UUID;
+
 public interface UserService {
-    public MessageResponseDTO signup (UserDTO user);
+
+    public MessageResponseDTO signup (UserDTO userDTO);
 
     // TokenDTO 추후 반환하는 식으로 수정하게 될 것 같습니다.
-    public MessageResponseDTO login (String username, String password);
+    public MessageResponseDTO login (LoginRequestDTO loginRequestDTO);
 
-    public MessageResponseDTO logout (UserDTO user);
+    public MessageResponseDTO logout (UserDTO userDTO);
 
-    public MessageResponseDTO updateUserInfo (UserDTO user);
+    public MessageResponseDTO updateUserInfo (UserDTO userDTO);
 
-    public MessageResponseDTO deleteUser (UserDTO user);
+    public MessageResponseDTO deleteUser (UUID userId);
 }
