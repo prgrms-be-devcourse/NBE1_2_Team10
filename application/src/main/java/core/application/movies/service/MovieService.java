a9ad351901e7db1ccf115e18e347f16793548fc3
hgh1472 <hgh1472@naver.com>
2024-10-03 14:03:43 +0900

    Refactor : 영화 검색어를 통한 검색과 장르 검색 구분
    Related:#63

--- application/src/main/java/core/application/movies/service/MovieService.java
+++ application/src/main/java/core/application/movies/service/MovieService.java
@@ -1,12 +1,12 @@
 package core.application.movies.service;
 
+import java.util.List;
+
 import core.application.movies.constant.Genre;
 import core.application.movies.constant.MovieSearch;
 import core.application.movies.models.dto.MainPageMoviesRespDTO;
 import core.application.movies.models.dto.MovieDetailRespDTO;
-import core.application.movies.models.dto.MovieSimpleRespDTO;
-
-import java.util.List;
+import core.application.movies.models.dto.MovieSearchRespDTO;
 
 public interface MovieService {
 
@@ -17,16 +17,24 @@ public interface MovieService {
 	public MainPageMoviesRespDTO getMainPageMovieInfo();
 
 	/**
-	 * 사용자는 장르, 검색어, 정렬 조건을 이용할 수 있다. <br>
-	 * 정렬 조건과 검색어는 필수이지만, 장르는 선택적으로 사용한다.
+	 * 통합 검색은 검색 후 정확도순, 최신순을 제공한다.
 	 *
 	 * @param page 검색 페이지 목록
 	 * @param sort 정렬 조건
 	 * @param query 검색어
-	 * @param genre 영화 장르
 	 * @return 검색한 영화 정보
 	 */
-	public List<MovieSimpleRespDTO> searchMovies(Integer page, MovieSearch sort, String query, Genre genre);
+	public List<MovieSearchRespDTO> searchMovies(Integer page, MovieSearch sort, String query);
+
+	/**
+	 * 카테고리에 해당하는 영화를 제공한다.
+	 *
+	 * @param page 페이지
+	 * @param genre 원하는 장르
+	 * @param sort 정렬 조건
+	 * @return 해당 카테고리의 영화
+	 */
+	public List<MovieSearchRespDTO> getMoviesWithGenre(Integer page, Genre genre, MovieSearch sort);
 
 	/**
 	 * 사용자가 영화 상세 페이지에서 필요로 하는 정보를 보내준다.
