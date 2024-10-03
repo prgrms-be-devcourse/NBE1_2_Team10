dcf97b4b20c1c745517014bc53c4e6614c359ab6
minjung <minjung0768@gmail.com>
2024-10-03 12:02:58 +0900

    Fix : token 유효 시간, UsrService 수정
    수정 token expired time 상수화
    추가 findUserByUserId 메소드
    수정 사용자 이메일 정보 금지
    
    Related to: #1, #2, #3, #4

--- application/src/main/java/core/application/users/service/UserService.java
+++ application/src/main/java/core/application/users/service/UserService.java
@@ -3,19 +3,23 @@ package core.application.users.service;
 import core.application.users.models.dto.LoginRequestDTO;
 import core.application.users.models.dto.MessageResponseDTO;
 import core.application.users.models.dto.UserDTO;
+import core.application.users.models.entities.UserEntity;
+import jakarta.servlet.http.HttpServletRequest;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserService {
 
     public MessageResponseDTO signup (UserDTO userDTO);
 
-    // TokenDTO 추후 반환하는 식으로 수정하게 될 것 같습니다.
     public MessageResponseDTO login (LoginRequestDTO loginRequestDTO);
 
-    public MessageResponseDTO logout (UserDTO userDTO);
+    public MessageResponseDTO logout ();
 
     public MessageResponseDTO updateUserInfo (UserDTO userDTO);
 
-    public MessageResponseDTO deleteUser (UUID userId);
+    public MessageResponseDTO deleteUser ();
+
+    public Optional<UserEntity> getUserByUserId(UUID userId);
 }
--- application/src/main/java/core/application/users/service/UserServiceImpl.java
+++ application/src/main/java/core/application/users/service/UserServiceImpl.java
@@ -6,7 +6,9 @@ import core.application.users.models.dto.UserDTO;
 import core.application.users.models.entities.UserEntity;
 import core.application.users.repositories.UserRepository;
 import core.application.users.repositories.UserRepositoryImpl;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +19,12 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final AuthenticatedUserInfo authenticatedUserInfo;
 
     @Autowired
-    public UserServiceImpl(UserRepositoryImpl userRepositoryImpl) {
+    public UserServiceImpl(UserRepositoryImpl userRepositoryImpl, AuthenticatedUserInfo authenticatedUserInfo) {
         this.userRepository = userRepositoryImpl;
+        this.authenticatedUserInfo = authenticatedUserInfo;
     }
 
     // 사용자 회원 가입
@@ -31,22 +35,23 @@ public class UserServiceImpl implements UserService {
         return new MessageResponseDTO(userEntity.get().getUserId(), "signUp success");
     }
 
-    // 사용자 로그인 // token 반환으로 추후 수정 필요
+    // 사용자 로그인 // token 반환으로 추후 수정 필요? header랑 이런데 들어가는데?? 일단 고민좀
     @Override
     public MessageResponseDTO login(LoginRequestDTO loginRequestDTO) {
         Optional<UserEntity> user = userRepository.findByUserEmailAndPassword(loginRequestDTO.getUserEmail(), loginRequestDTO.getUserPw());
         MessageResponseDTO messageResponseDTO;
-        if (!user.isPresent()) {
+        if (user.isEmpty()) {
             throw new IllegalArgumentException("Invalid email or password");
         }
         messageResponseDTO = new MessageResponseDTO(user.get().getUserId(), "login success");
         return messageResponseDTO;
     }
 
-    // 사용자 로그아웃
+    // 사용자 로그아웃 // 추후 구현
     @Override
-    public MessageResponseDTO logout(UserDTO userDTO) {
+    public MessageResponseDTO logout() {
         // token을 expired 리스트 만들어서 거기에 넣어두기 아마도? access token으로 어떻게 하는지 예시들 찾아보기
+        UUID userId = authenticatedUserInfo.getAuthenticatedUserId();
         return null;
     }
 
@@ -59,8 +64,14 @@ public class UserServiceImpl implements UserService {
 
     // 사용자 계정 삭제
     @Override
-    public MessageResponseDTO deleteUser(UUID userId) {
+    public MessageResponseDTO deleteUser() {
+        UUID userId = authenticatedUserInfo.getAuthenticatedUserId();
         userRepository.deleteUser(userId);
         return new MessageResponseDTO(userId, "user deleted successfully");
+    }
+
+    // 사용자 계정 정보 조회
+    public Optional<UserEntity> getUserByUserId(UUID userId) {
+        return userRepository.findByUserId(userId);
     }
 }
