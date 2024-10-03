04651262f44d10faa4734da11e8b576bc7001477
hgh1472 <hgh1472@naver.com>
2024-10-03 14:06:06 +0900

    Feat : 영화 통합검색 시 빈 결과 예외 처리
    Related:#63

---
+++ application/src/main/java/core/application/movies/controller/MovieExceptionAdvice.java
@@ -0,0 +1,23 @@
+package core.application.movies.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import core.application.movies.exception.ExceptionResult;
+import core.application.movies.exception.NoMovieException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class MovieExceptionAdvice {
+
+	@ResponseStatus(HttpStatus.OK)
+	@ExceptionHandler(NoMovieException.class)
+	public ExceptionResult handleNoMoviesException(NoMovieException e) {
+		log.error("KMDB API를 이용한 검색 결과 아무것도 나오지 않음.");
+		// 프론트에서 리턴 타입에 따라 처리가 가능할까?
+		return new ExceptionResult(e.getMessage());
+	}
+}
