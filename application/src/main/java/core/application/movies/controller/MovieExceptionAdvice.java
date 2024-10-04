1760e5db7661240fd928cf52f2bf3dd418999686
hgh1472 <hgh1472@naver.com>
2024-10-04 11:59:57 +0900

    Feat : 영화 장르 검색 매핑 추가 및 파라미터 default 값 설정
    Related:#63

--- application/src/main/java/core/application/movies/controller/MovieExceptionAdvice.java
+++ application/src/main/java/core/application/movies/controller/MovieExceptionAdvice.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import core.application.movies.exception.ExceptionResult;
 import core.application.movies.exception.NoMovieException;
+import core.application.movies.exception.WrongAccessException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -18,6 +19,13 @@ public class MovieExceptionAdvice {
 	public ExceptionResult handleNoMoviesException(NoMovieException e) {
 		log.error("KMDB API를 이용한 검색 결과 아무것도 나오지 않음.");
 		// 프론트에서 리턴 타입에 따라 처리가 가능할까?
+		return new ExceptionResult(e.getMessage());
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(WrongAccessException.class)
+	public ExceptionResult handleWrongAccessException(WrongAccessException e) {
+		log.error("잘못된 페이지 접근");
 		return new ExceptionResult(e.getMessage());
 	}
 }
