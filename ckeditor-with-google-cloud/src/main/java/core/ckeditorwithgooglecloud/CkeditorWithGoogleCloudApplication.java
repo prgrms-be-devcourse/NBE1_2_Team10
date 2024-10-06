5922222196317bf2de06d93557822dc172a53d18
청주는사과아님 <jbw9964@gmail.com>
2024-10-06 18:15:27 +0900

    build: `GCS` 사용하기 위한 build 설정
    - `com.google.cloud:spring-cloud-gcp-starter`
    - `com.google.cloud:spring-cloud-gcp-starter-storage`

---
+++ ckeditor-with-google-cloud/src/main/java/core/ckeditorwithgooglecloud/CkeditorWithGoogleCloudApplication.java
@@ -0,0 +1,13 @@
+package core.ckeditorwithgooglecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkeditorWithGoogleCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CkeditorWithGoogleCloudApplication.class, args);
+    }
+
+}
---
+++ ckeditor-with-google-cloud/src/test/java/core/ckeditorwithgooglecloud/CkeditorWithGoogleCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package core.ckeditorwithgooglecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkeditorWithGoogleCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
