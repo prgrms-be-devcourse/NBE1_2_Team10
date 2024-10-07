5aa012495a76638366198797c58b92bc16dc7805
hgh1472 <hgh1472@naver.com>
2024-10-07 21:56:36 +0900

    Feat : 한줄평 삭제 기능 추가 및 한줄평 등록 시 수정되어야 할 영화 데이터 수정
    Related:#80
    Ref:#57

--- application/src/main/java/core/application/movies/service/CommentService.java
+++ application/src/main/java/core/application/movies/service/CommentService.java
@@ -8,14 +8,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 import core.application.movies.constant.CommentSort;
 import core.application.movies.exception.InvalidReactionException;
+import core.application.movies.exception.InvalidWriteCommentException;
+import core.application.movies.exception.NoMovieException;
+import core.application.movies.exception.NotCommentWriterException;
 import core.application.movies.exception.NotFoundCommentException;
-import core.application.movies.exception.WrongWriteCommentException;
-import core.application.movies.models.dto.CommentRespDTO;
-import core.application.movies.models.dto.CommentWriteReqDTO;
+import core.application.movies.models.dto.request.CommentWriteReqDTO;
+import core.application.movies.models.dto.response.CommentRespDTO;
+import core.application.movies.models.entities.CachedMovieEntity;
 import core.application.movies.models.entities.CommentEntity;
-import core.application.movies.repositories.CommentDislikeRepository;
-import core.application.movies.repositories.CommentLikeRepository;
-import core.application.movies.repositories.CommentRepository;
+import core.application.movies.repositories.comment.CommentDislikeRepository;
+import core.application.movies.repositories.comment.CommentLikeRepository;
+import core.application.movies.repositories.comment.CommentRepository;
+import core.application.movies.repositories.movie.CachedMovieRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 public class CommentService {
+	private final CachedMovieRepository movieRepository;
 	private final CommentRepository commentRepository;
 	private final CommentLikeRepository likeRepository;
 	private final CommentDislikeRepository dislikeRepository;
@@ -32,7 +37,6 @@ public class CommentService {
 		if (sort.equals(CommentSort.LIKE)) {
 			return commentRepository.findByMovieIdOnLikeDescend(movieId, userId, page * 10);
 		}
-
 		if (sort.equals(CommentSort.LATEST)) {
 			return commentRepository.findByMovieIdOnDateDescend(movieId, userId, page * 10);
 		}
@@ -43,12 +47,31 @@ public class CommentService {
 	public CommentRespDTO writeCommentOnMovie(CommentWriteReqDTO writeReqDTO, UUID userId, String movieId) {
 		// 이미 작성한 기록이 있는지 확인한다.
 		if (commentRepository.existsByMovieIdAndUserId(movieId, userId)) {
-			throw new WrongWriteCommentException("한줄평은 1회 작성만 가능합니다.");
+			throw new InvalidWriteCommentException("한줄평은 1회 작성만 가능합니다.");
 		}
-
 		CommentEntity newComment = CommentEntity.of(writeReqDTO, movieId, userId);
 		CommentEntity save = commentRepository.saveNewComment(movieId, userId, newComment);
+		CachedMovieEntity movie = movieRepository.findByMovieId(movieId)
+			.orElseThrow(() -> new NoMovieException("존재하지 않는 영화입니다."));
+		log.info("수정 전 영화 총 평점 : {}, 수정 전 영화 한줄평 개수 : {}", movie.getSumOfRating(), movie.getCommentCount());
+		movie.isCommentedWithRating(newComment.getRating());
+		log.info("수정된 영화 총 평점 : {}, 수정된 영화 한줄평 개수 : {}", movie.getSumOfRating(), movie.getCommentCount());
+		movieRepository.editMovie(movieId, movie);
 		return CommentRespDTO.from(save);
+	}
+
+	@Transactional
+	public void deleteCommentOnMovie(String movieId, UUID userId, Long commentId) {
+		CommentEntity comment = commentRepository.findByCommentId(commentId)
+			.orElseThrow(() -> new NotFoundCommentException("존재하지 않는 한줄평입니다."));
+		if (!comment.getUserId().equals(userId)) {
+			throw new NotCommentWriterException("한줄평 작성자가 아닙니다.");
+		}
+		commentRepository.deleteComment(commentId);
+		CachedMovieEntity movie = movieRepository.findByMovieId(movieId)
+			.orElseThrow(() -> new NoMovieException("존재하는 영화가 아닙니다."));
+		movie.deleteComment(comment.getRating());
+		movieRepository.editMovie(movieId, movie);
 	}
 
 	@Transactional
@@ -58,7 +81,6 @@ public class CommentService {
 		if (likeRepository.isExistLike(commentId, userId)) {
 			throw new InvalidReactionException("이미 '좋아요'를 누른 한줄평입니다.");
 		}
-
 		comment.isLiked();
 		commentRepository.update(comment);
 		likeRepository.saveCommentLike(commentId, userId);
@@ -71,7 +93,6 @@ public class CommentService {
 		if (!likeRepository.isExistLike(commentId, userId)) {
 			throw new InvalidReactionException("'좋아요'를 누르지 않은 한줄평입니다.");
 		}
-
 		comment.cancelLike();
 		commentRepository.update(comment);
 		likeRepository.deleteCommentLike(commentId, userId);
@@ -84,7 +105,6 @@ public class CommentService {
 		if (dislikeRepository.isExistDislike(commentId, userId)) {
 			throw new InvalidReactionException("이미 '싫어요'를 누른 한줄평입니다.");
 		}
-
 		comment.isDisliked();
 		commentRepository.update(comment);
 		dislikeRepository.saveCommentDislike(commentId, userId);
@@ -97,7 +117,6 @@ public class CommentService {
 		if (!dislikeRepository.isExistDislike(commentId, userId)) {
 			throw new InvalidReactionException("'싫어요'를 누르지 않은 한줄평입니다.");
 		}
-
 		comment.cancelDislike();
 		commentRepository.update(comment);
 		dislikeRepository.deleteCommentDislike(commentId, userId);
