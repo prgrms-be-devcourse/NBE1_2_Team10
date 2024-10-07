0e83bc8d76849c6b1532b5af6f199ca0f7296c7f
hgh1472 <hgh1472@naver.com>
2024-10-07 21:57:57 +0900

    Refactor : 패키지명 변경에 따른 import 수정
    Related:#80

--- application/src/main/java/core/application/movies/service/MovieService.java
+++ application/src/main/java/core/application/movies/service/MovieService.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import core.application.movies.constant.Genre;
 import core.application.movies.constant.MovieSearch;
-import core.application.movies.models.dto.MainPageMoviesRespDTO;
-import core.application.movies.models.dto.MovieDetailRespDTO;
-import core.application.movies.models.dto.MovieSearchRespDTO;
+import core.application.movies.models.dto.response.MainPageMoviesRespDTO;
+import core.application.movies.models.dto.response.MovieDetailRespDTO;
+import core.application.movies.models.dto.response.MovieSearchRespDTO;
 
 public interface MovieService {
 
@@ -26,15 +26,9 @@ public interface MovieService {
 	 */
 	public List<MovieSearchRespDTO> searchMovies(Integer page, MovieSearch sort, String query);
 
-	/**
-	 * 카테고리에 해당하는 영화를 제공한다.
-	 *
-	 * @param page 페이지
-	 * @param genre 원하는 장르
-	 * @param sort 정렬 조건
-	 * @return 해당 카테고리의 영화
-	 */
-	public List<MovieSearchRespDTO> getMoviesWithGenre(Integer page, Genre genre, MovieSearch sort);
+	public List<MovieSearchRespDTO> getMoviesWithGenreRatingOrder(Integer page, Genre genre);
+
+	public List<MovieSearchRespDTO> getMoviesWithGenreLatestOrder(Integer page, Genre genre);
 
 	/**
 	 * 사용자가 영화 상세 페이지에서 필요로 하는 정보를 보내준다.
