f9838a1fd424ace78f04039d27f9680c41ce5b60
청주는사과아님 <jbw9964@gmail.com>
2024-10-08 20:34:57 +0900

    test: `ReviewService` 구현체 테스트 코드 추가

---
+++ application/src/test/java/core/application/reviews/services/ReviewServiceImplTest.java
@@ -0,0 +1,252 @@
+package core.application.reviews.services;
+
+import static java.util.Comparator.comparing;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.when;
+
+import core.application.reviews.exceptions.NoReviewFoundException;
+import core.application.reviews.models.entities.ReviewEntity;
+import core.application.reviews.repositories.ReviewRepository;
+import java.time.Instant;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.LongStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class ReviewServiceImplTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ReviewServiceImplTest.class);
+    @MockBean
+    ReviewRepository reviewRepo;
+
+    @Autowired
+    ReviewService reviewService;
+
+    private static final int TEST_SIZE = 10;
+    private static final String movieId = "test-12345";
+
+    private static final Comparator<ReviewEntity> latest
+            = comparing(ReviewEntity::getCreatedAt).reversed()
+            .thenComparing(comparing(ReviewEntity::getMovieId).reversed());
+    private static final Comparator<ReviewEntity> like
+            = comparing(ReviewEntity::getLike).reversed()
+            .thenComparing(comparing(ReviewEntity::getMovieId).reversed());
+
+    private static List<ReviewEntity> testReviews;
+    private static List<ReviewEntity> emptyTestReviews;
+
+    private ReviewEntity genEntity(
+            Long id, String movieId, String title, String content,
+            int likes, Instant created, Instant updated) {
+        return ReviewEntity.builder()
+                .reviewId(id)
+                .movieId(movieId)
+                .title(title)
+                .content(content)
+                .like(likes)
+                .createdAt(created)
+                .updatedAt(updated)
+                .build();
+    }
+
+    @FunctionalInterface
+    private interface Triplet<T1, T2, T3, R> {
+
+        R apply(T1 t1, T2 t2, T3 t3);
+    }
+
+    private void assertSortedSelection(
+            Triplet<String, Integer, Integer, List<ReviewEntity>> triplet,
+            Comparator<ReviewEntity> sortOrder,
+            List<ReviewEntity> samples, String movieId) {
+        when(triplet.apply(eq(movieId), anyInt(), anyInt())).thenAnswer(invocation -> {
+            int offset = invocation.getArgument(1, Integer.class);
+            int limit = invocation.getArgument(2, Integer.class);
+
+            return samples.stream().sorted(sortOrder).skip(offset).limit(limit).toList();
+        });
+    }
+
+    @BeforeEach
+    void setUp() {
+        Random random = new Random();
+        testReviews = LongStream.range(0, TEST_SIZE)
+                .boxed()
+                .map(i -> genEntity(i, movieId, "Test review title", "Test content",
+                        TEST_SIZE - i.intValue(), Instant.now(),
+                        random.nextBoolean() ? Instant.now() : null))
+                .toList();
+
+        emptyTestReviews = testReviews.stream()
+                .map(t -> genEntity(t.getReviewId(), t.getMovieId(), t.getTitle(),
+                        null, t.getLike(), t.getCreatedAt(), t.getUpdatedAt()))
+                .toList();
+
+        testReviews.forEach(t -> when(reviewRepo.findByReviewId(t.getReviewId()))
+                .thenReturn(Optional.of(t)));
+
+        emptyTestReviews.forEach(
+                t -> when(reviewRepo.findByReviewIdWithoutContent(t.getReviewId()))
+                        .thenReturn(Optional.of(t)));
+
+        assertSortedSelection(reviewRepo::findByMovieIdOnDateDescend, latest, testReviews, movieId);
+        assertSortedSelection(reviewRepo::findByMovieIdOnLikeDescend, like, testReviews, movieId);
+
+        assertSortedSelection(reviewRepo::findByMovieIdWithoutContentOnDateDescend, latest,
+                emptyTestReviews, movieId);
+        assertSortedSelection(reviewRepo::findByMovieIdWithoutContentOnLikeDescend, like,
+                emptyTestReviews, movieId);
+    }
+
+    @Test
+    @DisplayName("특정 영화에 달린 포스팅 목록을 확인.")
+    void getReviewsOnMovieId() {
+
+        int num = 5;
+        for (int offset = 0; offset + num <= TEST_SIZE; offset += TEST_SIZE / 3) {
+
+            for (ReviewSortOrder order : ReviewSortOrder.values()) {
+
+                List<ReviewEntity> withContent = reviewService.getReviewsOnMovieId(movieId, order,
+                        true, offset, num);
+                List<ReviewEntity> withoutContent = reviewService.getReviewsOnMovieId(movieId,
+                        order, false, offset, num);
+
+                log.info("Check whether given list has correct size.");
+
+                assertThat(withContent).hasSize(num);
+                assertThat(withoutContent).hasSize(num);
+
+                Comparator<ReviewEntity> correctOrder
+                        = order == ReviewSortOrder.LATEST ? latest :
+                        order == ReviewSortOrder.LIKE ? like : null;
+
+                if (correctOrder == null) {
+                    throw new RuntimeException();
+                }
+
+                List<ReviewEntity> correctAnswerWithContent
+                        = testReviews.stream().sorted(correctOrder).toList();
+                List<ReviewEntity> correctAnswerWithOutContent
+                        = emptyTestReviews.stream().sorted(correctOrder).toList();
+
+                log.info("Check whether given list maintains order.");
+                assertThat(withContent).isSortedAccordingTo(correctOrder);
+                assertThat(withoutContent).isSortedAccordingTo(correctOrder);
+
+                log.info("Check whether correct subset were given.");
+                assertThat(correctAnswerWithContent).containsAll(withContent);
+                assertThat(correctAnswerWithOutContent).containsAll(withoutContent);
+
+                log.info("Check whether given list's properties are valid.");
+                withContent.forEach(r -> assertThat(r.getContent()).isNotNull());
+                withoutContent.forEach(r -> assertThat(r.getContent()).isNull());
+
+                log.info("LGTM!");
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("한 리뷰의 상세 정보를 가져오기.")
+    void getReviewInfo() {
+
+        log.info("Check whether method run correctly.");
+        testReviews.forEach(t -> {
+            ReviewEntity result = reviewService.getReviewInfo(t.getReviewId(), true);
+
+            assertThat(result).isNotNull();
+            assertThat(result).isEqualTo(t);
+        });
+
+        emptyTestReviews.forEach(t -> {
+            ReviewEntity result = reviewService.getReviewInfo(t.getReviewId(), false);
+
+            assertThat(result).isNotNull();
+            assertThat(result.getContent()).isNull();
+            assertThat(result).isEqualTo(t);
+        });
+
+        Random random = new Random();
+
+        log.info("Check whether exception were thrown with invalid argument.");
+        assertThatThrownBy(() -> reviewService.getReviewInfo(random.nextLong(), true))
+                .isInstanceOf(
+                        NoReviewFoundException.class);
+
+        log.info("good!");
+    }
+
+    @Test
+    @DisplayName("포스팅 정보를 수정한다.")
+    void updateReviewInfo() {
+
+        testReviews.forEach(t ->
+                when(reviewRepo.editReviewInfo(eq(t.getReviewId()),
+                        any(ReviewEntity.class))).thenAnswer(invocation -> {
+                    ReviewEntity arg = invocation.getArgument(1, ReviewEntity.class);
+
+                    return genEntity(t.getReviewId(), t.getMovieId(), arg.getTitle(),
+                            arg.getContent(), t.getLike(), t.getUpdatedAt(), Instant.now());
+                })
+        );
+
+        ReviewEntity replacement = ReviewEntity.builder()
+                .title("replacement")
+                .content("replacement-content")
+                .build();
+
+        testReviews.forEach(t -> {
+            ReviewEntity result = reviewService.updateReviewInfo(t.getReviewId(), replacement);
+
+            assertThat(result).satisfies(
+                    r -> assertThat(r.getTitle()).isEqualTo(replacement.getTitle()),
+                    r -> assertThat(r.getContent()).isEqualTo(replacement.getContent()),
+                    r -> assertThat(r.getReviewId()).isEqualTo(t.getReviewId()),
+                    r -> assertThat(r.getMovieId()).isEqualTo(t.getMovieId())
+            );
+        });
+
+        Random random = new Random();
+
+        assertThatThrownBy(() -> reviewService.updateReviewInfo(random.nextLong(), replacement))
+                .isInstanceOf(NoReviewFoundException.class);
+    }
+
+    @Test
+    @DisplayName("포스팅을 삭제한다.")
+    void deleteReview() {
+        Random random = new Random();
+
+        assertThatThrownBy(() -> reviewService.deleteReview(random.nextLong()))
+                .isInstanceOf(NoReviewFoundException.class);
+    }
+
+    @Test
+    @DisplayName("포스팅의 좋아요를 증감시킨다.")
+    void testLikes() {
+        Random random = new Random();
+
+        assertThatThrownBy(() -> reviewService.increaseLikes(random.nextLong(), null))
+                .isInstanceOf(NoReviewFoundException.class);
+
+        assertThatThrownBy(() -> reviewService.decreaseLikes(random.nextLong(), null))
+                .isInstanceOf(NoReviewFoundException.class);
+    }
+}
