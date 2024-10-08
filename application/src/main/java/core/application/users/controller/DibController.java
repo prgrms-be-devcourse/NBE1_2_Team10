dda79cd494ace1ff207addae289531a7340381ed
hgh1472 <hgh1472@naver.com>
2024-10-08 16:50:49 +0900

    Docs : `user` 도메인 Swagger 적용

--- application/src/main/java/core/application/users/controller/DibController.java
+++ application/src/main/java/core/application/users/controller/DibController.java
@@ -1,7 +1,10 @@
 package core.application.users.controller;
 
+import core.application.api.response.ApiResponse;
 import core.application.users.models.dto.DibRespDTO;
 import core.application.users.service.DibService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -12,13 +15,16 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/movies")
+@Tag(name = "Dib", description = "영화 찜 API")
 public class DibController {
 
     private final DibService dibService;
 
+    @Operation(summary = "영화 찜 등록/취소")
     @PutMapping("/{movieId}/dib")
-    public DibRespDTO dibPRocess(@PathVariable String movieId) {
+    public ApiResponse<DibRespDTO> dibPRocess(@PathVariable String movieId) {
         UUID userId = UUID.fromString("991c95d6-808a-11ef-8da5-467268b55380");
-        return dibService.dibProcess(userId, movieId);
+        DibRespDTO dibRespDTO = dibService.dibProcess(userId, movieId);
+        return ApiResponse.onSuccess(dibRespDTO);
     }
 }
