8ed9279b206f684f4f853bd21db36c8386e99dc1
minjung <minjung0768@gmail.com>
2024-10-08 14:16:53 +0900

    Feat : 수정 userDTO 공백 포함 못하게 수정
    userDTO에 들어가는 내용들에 공백 포함이 되지 않게 수정

--- application/src/main/java/core/application/users/models/dto/UserDTO.java
+++ application/src/main/java/core/application/users/models/dto/UserDTO.java
@@ -16,15 +16,23 @@ import java.util.UUID;
 
 public class UserDTO {
     private UUID userId;
+
     @Email
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message="이메일 주소 양식을 확인해주세요")
     private String userEmail;
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$")
+
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$", message = "비밀번호는 8자 이상 15자 이하이며, 영문자, 숫자 및 특수문자를 포함해야 합니다.")
     private String userPw;
+
     private UserRole role;
+
+    @Pattern(regexp = "^(?!.*\\s).+$", message = "별명에는 공백을 포함할 수 없습니다.")
     private String alias;
-    @Pattern(regexp = "^010-\\d{4}-\\d{4}$")
+
+    @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "전화번호 형식이 맞지 않습니다.")
     private String phoneNum;
+
+    @Pattern(regexp = "^(?!.*\\s).+$", message = "이름에는 공백을 포함할 수 없습니다.")
     private String userName;
 
     // UserDTO -> UserEntity 변환 메서드
