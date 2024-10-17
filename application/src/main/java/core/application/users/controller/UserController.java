205ed36c4f45255f1c687e19dacbbd0a9e874039
minjung <minjung0768@gmail.com>
2024-10-17 15:43:53 +0900

    Fix : 추가 회원 정보 수정 DTO
    회원 정보 수정 전용 dto가 필요해서 추가함
    기존에는 회원 가입과 정보 수정에 동일한 DTO를 사용했지만 notnull 여부와
    role 필요 여부에 따라 새 dto가 필요하다고 생각함
    role은 관리자만 바꿀 수 있으므로 dto에서 받는 요소에서 삭제함
    email은 변경은 불가하지만 token의 이메일과 바꾸고자하는 회원정보의 회원
    동일 여부를 판단하기 위해 필요한 요소

--- application/src/main/java/core/application/users/controller/UserController.java
+++ application/src/main/java/core/application/users/controller/UserController.java
@@ -4,7 +4,8 @@ import core.application.api.response.ApiResponse;
 import core.application.api.response.code.Message;
 import core.application.users.models.dto.MessageResponseDTO;
 import core.application.security.service.TokenService;
-import core.application.users.models.dto.UserRequestDTO;
+import core.application.users.models.dto.SignupReqDTO;
+import core.application.users.models.dto.UserUpdateReqDTO;
 import core.application.users.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -55,7 +56,7 @@ public class UserController {
 
     @Operation(summary = "회원가입")
     @PostMapping("/signup")
-    public ApiResponse<MessageResponseDTO> singUp(@Valid @RequestBody UserRequestDTO userRequestDTO) {
+    public ApiResponse<MessageResponseDTO> singUp(@Valid @RequestBody SignupReqDTO userRequestDTO) {
         MessageResponseDTO messageResponseDTO = userService.signup(userRequestDTO);
         return ApiResponse.onCreateSuccess(messageResponseDTO);
     }
@@ -78,8 +79,8 @@ public class UserController {
 
     @Operation(summary = "유저 정보 업데이트")
     @PatchMapping("/update")
-    public ApiResponse<MessageResponseDTO> updateUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
-        MessageResponseDTO messageResponseDTO = userService.updateUserInfo(userRequestDTO);
+    public ApiResponse<MessageResponseDTO> updateUser(@Valid @RequestBody UserUpdateReqDTO userUpdateRequestDTO) {
+        MessageResponseDTO messageResponseDTO = userService.updateUserInfo(userUpdateRequestDTO);
         return ApiResponse.onSuccess(messageResponseDTO);
     }
 
