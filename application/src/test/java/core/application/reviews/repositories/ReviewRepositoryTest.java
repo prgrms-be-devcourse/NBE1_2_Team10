dc6bbac28bf32a616bcf15a5612c0dfa5b446e25
jbw9964 <jbw9964@gmail.com>
2024-10-18 14:19:36 +0900

    test: `ReviewRepository` 테스트 코드 보완
    - `User`, `CachedMovie` `Repository` 와 이용해서 어떤 DB 에서든 테스트 돌아가도록 수정

--- application/src/test/java/core/application/reviews/repositories/ReviewRepositoryTest.java
+++ application/src/test/java/core/application/reviews/repositories/ReviewRepositoryTest.java
@@ -1,28 +1,22 @@
 package core.application.reviews.repositories;
 
-import static java.util.Comparator.comparing;
-import static org.assertj.core.api.Assertions.assertThat;
+import static java.util.Comparator.*;
+import static org.assertj.core.api.Assertions.*;
 
-import core.application.movies.models.entities.CachedMovieEntity;
-import core.application.movies.repositories.movie.CachedMovieRepository;
-import core.application.reviews.models.entities.ReviewEntity;
-import core.application.users.models.entities.UserEntity;
-import core.application.users.models.entities.UserRole;
-import core.application.users.repositories.UserRepository;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Comparator;
-import java.util.List;
-import java.util.UUID;
-import java.util.logging.Logger;
-import java.util.stream.IntStream;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import core.application.movies.models.entities.*;
+import core.application.movies.repositories.movie.*;
+import core.application.reviews.models.entities.*;
+import core.application.users.models.entities.*;
+import core.application.users.repositories.*;
+import java.time.*;
+import java.time.temporal.*;
+import java.util.*;
+import java.util.logging.*;
+import java.util.stream.*;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.context.*;
+import org.springframework.transaction.annotation.*;
 
 @SpringBootTest
 @Transactional
@@ -39,31 +33,43 @@ class ReviewRepositoryTest {
     @Autowired
     private CachedMovieRepository movieRepo;
 
-    private final Comparator<ReviewEntity> dateDescend
-            = comparing(ReviewEntity::getCreatedAt).reversed()
-            .thenComparing(comparing(ReviewEntity::getReviewId).reversed());
-    private final Comparator<ReviewEntity> likeDescend
-            = comparing(ReviewEntity::getLike).reversed()
-            .thenComparing(comparing(ReviewEntity::getReviewId).reversed());
+    // 테스팅 용 user, movie
+    private static final String TESTING = "TESTING TESTING";
+    private static UserEntity testUser = UserEntity.builder()
+            .userEmail(TESTING)
+            .userName(TESTING)
+            .role(UserRole.USER)
+            .userPw(TESTING)
+            .build();
+    private static CachedMovieEntity testMovie = CachedMovieEntity.builder()
+            .movieId(TESTING)
+            .title(TESTING)
+            .posterUrl(TESTING)
+            .genre(TESTING)
+            .releaseDate(TESTING)
+            .plot(TESTING)
+            .runningTime("1234")    // DB 에 길이 제한 있어서 이렇게
+            .actors(TESTING)
+            .director(TESTING)
+            .dibCount(0L)
+            .reviewCount(0L)
+            .commentCount(0L)
+            .sumOfRating(0L)
+            .build();
 
+    /**
+     * 테스팅 용 review 목록들 (페이징, 정렬 상태 확인용)
+     */
+    private static List<ReviewEntity> testReviews;
+    private static final int TEST_SIZE = 100;
 
-    // DB 에 존재하는 유저 id
-    // Table DDL 에서 UUID 가 generate 되는게 아니라 DEFAULT EXPRESSION 으로 생성되서
-    // MyBatis useGeneratedKey 인식을 못함
-    // 그래서 나중에 saveNewUser 메서드 UUID 도 제공 받아서 저장되도록 만들어야됨 tq
-    private static final UUID userId = UUID.fromString("01cbb144-851e-11ef-b878-00d861a152a7");
-    private static UserEntity testUser;
-    private static CachedMovieEntity testMovie;
-
-    private static List<ReviewEntity> testReviews1;
-    private static List<ReviewEntity> testReviews2;
-    private static final int TEST_SIZE = 10;
-
-
+    /**
+     * 손쉽게 review 엔티티 만들기
+     */
     private static ReviewEntity genReview(UUID userId, String movieId, int like) {
         return ReviewEntity.builder()
-                .title("Testing")
-                .content("Testing")
+                .title(TESTING)
+                .content(TESTING)
                 .like(like)
                 .userId(userId)
                 .movieId(movieId)
@@ -71,36 +77,28 @@ class ReviewRepositoryTest {
                 .build();
     }
 
-    @BeforeAll
-    static void setUpBeforeClass() throws Exception {
-        testUser = UserEntity.builder()
-                .userId(userId)
-                .userEmail("review-repo-testing-@test.com")
-                .userName("review-repo-testing")
-                .role(UserRole.USER)
-                .userPw("testing")
-                .build();
-
-        testMovie = CachedMovieEntity.builder()
-                .movieId("review-repo-testing-movie")
-                .title("review-repo-testing-movie")
-                .posterUrl("review-repo-testing-movie")
-                .dibCount(0L)
-                .reviewCount(0L)
-                .commentCount(0L)
-                .sumOfRating(0L)
-                .build();
-    }
+    // 검사해야 할 order 들
+    private final Comparator<ReviewEntity> dateDescend
+            = comparing(ReviewEntity::getCreatedAt).reversed()
+            .thenComparing(comparing(ReviewEntity::getReviewId).reversed());
+    private final Comparator<ReviewEntity> likeDescend
+            = comparing(ReviewEntity::getLike).reversed()
+            .thenComparing(comparing(ReviewEntity::getReviewId).reversed());
 
     @BeforeEach
     void setUp() {
-        try {
-            movieRepo.saveNewMovie(testMovie);
-        } catch (Exception e) {
-            log.warning(e.getMessage());
-        }
+        // DB 저장된 거 있으면 가져오고 없으면 생성해서 가져옴
+        testUser = userRepo.findByUserEmail(testUser.getUserEmail())
+                .orElseGet(() -> {
+                    userRepo.saveNewUser(testUser);
+                    return userRepo.findByUserEmail(testUser.getUserEmail()).orElseThrow();
+                });
 
-        testReviews1 = IntStream.range(0, TEST_SIZE).parallel()
+        testMovie = movieRepo.findByMovieId(testMovie.getMovieId())
+                .orElseGet(() -> movieRepo.saveNewMovie(testMovie));
+
+        // testUser, testMovie 정보로 임의 리뷰 만듬
+        testReviews = IntStream.range(0, TEST_SIZE).parallel()
                 .boxed()
                 .map(i -> genReview(testUser.getUserId(), testMovie.getMovieId(), TEST_SIZE - i))
                 .toList();
@@ -109,20 +107,32 @@ class ReviewRepositoryTest {
     @Test
     @DisplayName("새 리뷰 글을 만든다.")
     void saveNewReview() {
+        log.info("<- saveNewReview");
+
+        // 제대로 insert 할 엔티티 생성
         ReviewEntity testReview = genReview(testUser.getUserId(), testMovie.getMovieId(), 0);
 
+        // DB 저장
         ReviewEntity result = reviewRepo.saveNewReview(testReview.getMovieId(),
                 testReview.getUserId(), testReview);
 
-        assertThat(result.getReviewId()).isNotNull();
-        assertThat(testReview.getReviewId()).isNotNull();
+        // 확인
+        assertThat(result).satisfies(
+                r -> assertThat(r).isNotNull(),
+                r -> assertThat(r.getReviewId()).isNotNull(),
+                r -> assertThat(r.getUserId()).isEqualTo(testUser.getUserId()).isNotNull(),
+                r -> assertThat(r.getMovieId()).isEqualTo(testMovie.getMovieId()).isNotNull()
+        );
 
-        log.info(result.toString());
+        log.fine(result.toString());
+        log.info("-> saveNewReview");
     }
 
     @Test
     @DisplayName("리뷰글 ID 로 찾는다.")
     void findByReviewId() {
+        log.info("<- findByReviewId");
+
         ReviewEntity testReview = reviewRepo.saveNewReview(
                 testMovie.getMovieId(), testUser.getUserId(),
                 genReview(null, null, 0));
@@ -130,39 +140,62 @@ class ReviewRepositoryTest {
         ReviewEntity result = reviewRepo.findByReviewId(testReview.getReviewId()).orElseThrow();
 
         assertThat(testReview.equals(result)).isTrue();
+
+        // 없을 땐 empty
+        long random = new Random().nextLong();
+        assertThat(reviewRepo.findByReviewId(random)).isEmpty();
+
+        log.info("-> findByReviewId");
     }
 
     @Test
     @DisplayName("특정 영화에 달린 리뷰글을 보여준다.")
     void findByMovieId() {
-        List<ReviewEntity> testList = testReviews1.stream()
+        log.info("<- findByMovieId");
+
+        // testReviews DB 에 저장, sorted 해서 변수로 가져옴
+        List<ReviewEntity> testList = testReviews.stream()
                 .map(t -> reviewRepo.saveNewReview(t.getMovieId(), t.getUserId(), t))
                 .sorted(Comparator.comparing(ReviewEntity::getReviewId).reversed())
                 .toList();
 
-        List<ReviewEntity> result1 = reviewRepo.findByMovieId(testMovie.getMovieId(), 0,
-                TEST_SIZE / 2);
+        testList.forEach(t -> log.fine(t.toString()));
 
-        testList.forEach(t -> log.info(t.toString()));
+        // DB 에서 페이징 된 거 가져옴
+        List<ReviewEntity> result1 = reviewRepo.findByMovieId(testMovie.getMovieId(),
+                0, TEST_SIZE / 2);
 
+        // 원하는 개수 만큼, 내용 제대로 가져왔는지 확인
         assertThat(result1).hasSize(TEST_SIZE / 2);
         assertThat(testList).containsAll(result1);
 
-        result1.forEach(t -> log.info(t.toString()));
+        result1.forEach(t -> log.fine(t.toString()));
 
-        List<ReviewEntity> result2 = reviewRepo.findByMovieId(testMovie.getMovieId(), TEST_SIZE / 2,
-                TEST_SIZE / 2);
+        List<ReviewEntity> result2 = reviewRepo.findByMovieId(testMovie.getMovieId(),
+                TEST_SIZE / 2, TEST_SIZE / 2);
 
         assertThat(result2).hasSize(TEST_SIZE / 2);
         assertThat(testList).containsAll(result2);
 
+        result2.forEach(t -> log.fine(t.toString()));
+
+        // result1 이랑 result2 겹치는 거 없는지 확인
         assertThat(result1).doesNotContainAnyElementsOf(result2);
+
+        // 없을 땐 empty
+        String random = "RANDOM RANDOM";
+        assertThat(reviewRepo.findByMovieId(random, 0, TEST_SIZE)).isEmpty();
+
+        log.info("-> findByMovieId");
     }
 
     @Test
     @DisplayName("특정 영화에 달린 리뷰글을 최신순으로 보여준다.")
     void findByMovieIdOnDateDescend() {
-        List<ReviewEntity> testList = testReviews1.stream()
+        log.info("<- findByMovieIdOnDateDescend");
+
+        // DB 에 리뷰글 저장
+        List<ReviewEntity> testList = testReviews.stream()
                 .map(t -> reviewRepo.saveNewReview(t.getMovieId(), t.getUserId(), t))
                 .sorted(dateDescend)
                 .toList();
@@ -172,10 +205,11 @@ class ReviewRepositoryTest {
         List<ReviewEntity> result2 = reviewRepo.findByMovieIdOnDateDescend(testMovie.getMovieId(),
                 TEST_SIZE / 2, TEST_SIZE / 2);
 
-        testList.forEach(t -> log.info(t.toString()));
-        result1.forEach(t -> log.info(t.toString()));
-        result2.forEach(t -> log.info(t.toString()));
+        testList.forEach(t -> log.fine(t.toString()));
+        result1.forEach(t -> log.fine(t.toString()));
+        result2.forEach(t -> log.fine(t.toString()));
 
+        // 가져온 개수, 내용, 정렬 잘 됐는지 확인
         assertThat(result1).hasSize(TEST_SIZE / 2);
         assertThat(testList).containsAll(result1);
         assertThat(result1).isSortedAccordingTo(dateDescend);
@@ -184,13 +218,23 @@ class ReviewRepositoryTest {
         assertThat(testList).containsAll(result2);
         assertThat(result2).isSortedAccordingTo(dateDescend);
 
+        // 겹치는 거 없는지 확인
         assertThat(result1).doesNotContainAnyElementsOf(result2);
+
+        // 없을 땐 empty
+        String random = "RANDOM RANDOM";
+        assertThat(reviewRepo.findByMovieIdOnDateDescend(random, 0, TEST_SIZE)).isEmpty();
+
+        log.info("-> findByMovieIdOnDateDescend");
     }
 
     @Test
     @DisplayName("특정 영화에 달린 리뷰글을 좋아요 순으로 보여준다.")
     void findByMovieIdOnLikeDescend() {
-        List<ReviewEntity> testList = testReviews1.stream()
+        log.info("<- findByMovieIdOnLikeDescend");
+
+        // DB 에 리뷰들 저장
+        List<ReviewEntity> testList = testReviews.stream()
                 .map(t -> reviewRepo.saveNewReview(t.getMovieId(), t.getUserId(), t))
                 .sorted(likeDescend)
                 .toList();
@@ -200,10 +244,11 @@ class ReviewRepositoryTest {
         List<ReviewEntity> result2 = reviewRepo.findByMovieIdOnLikeDescend(testMovie.getMovieId(),
                 TEST_SIZE / 2, TEST_SIZE / 2);
 
-        testList.forEach(t -> log.info(t.toString()));
-        result1.forEach(t -> log.info(t.toString()));
-        result2.forEach(t -> log.info(t.toString()));
+        testList.forEach(t -> log.fine(t.toString()));
+        result1.forEach(t -> log.fine(t.toString()));
+        result2.forEach(t -> log.fine(t.toString()));
 
+        // 가져온 개수, 내용, 정렬 잘 됐는지 확인
         assertThat(result1).hasSize(TEST_SIZE / 2);
         assertThat(testList).containsAll(result1);
         assertThat(result1).isSortedAccordingTo(likeDescend);
@@ -212,13 +257,23 @@ class ReviewRepositoryTest {
         assertThat(testList).containsAll(result2);
         assertThat(result2).isSortedAccordingTo(likeDescend);
 
+        // 겹치는 거 없는지 확인
         assertThat(result1).doesNotContainAnyElementsOf(result2);
+
+        // 없을 땐 empty
+        String random = "RANDOM RANDOM";
+        assertThat(reviewRepo.findByMovieIdOnLikeDescend(random, 0, TEST_SIZE)).isEmpty();
+
+        log.info("-> findByMovieIdOnLikeDescend");
     }
 
     @Test
     @DisplayName("특정 영화에 달린 리뷰글을 본문을 제외하고 불러온다.")
     void findByMovieIdWithoutContent() {
-        List<ReviewEntity> testList = testReviews1.stream()
+        log.info("<- findByMovieIdWithoutContent");
+
+        // DB 에 리뷰들 저장
+        List<ReviewEntity> testList = testReviews.stream()
                 .map(t -> reviewRepo.saveNewReview(t.getMovieId(), t.getUserId(), t))
                 .sorted(Comparator.comparing(ReviewEntity::getReviewId).reversed())
                 .toList();
@@ -228,20 +283,30 @@ class ReviewRepositoryTest {
         List<ReviewEntity> result2 = reviewRepo.findByMovieIdWithoutContent(testMovie.getMovieId(),
                 TEST_SIZE / 2, TEST_SIZE / 2);
 
-        testList.forEach(t -> log.info(t.toString()));
-        result1.forEach(t -> log.info(t.toString()));
-        result2.forEach(t -> log.info(t.toString()));
+        testList.forEach(t -> log.fine(t.toString()));
+        result1.forEach(t -> log.fine(t.toString()));
+        result2.forEach(t -> log.fine(t.toString()));
 
+        // 개수 잘 가져왓는지, 본문은 null 인지 확인
         assertThat(result1).hasSize(TEST_SIZE / 2);
         result1.forEach(r -> assertThat(r.getContent()).isNull());
 
         assertThat(result2).hasSize(TEST_SIZE / 2);
         result2.forEach(t -> assertThat(t.getContent()).isNull());
+
+        // 없을 땐 empty
+        String random = "RANDOM RANDOM";
+        assertThat(reviewRepo.findByMovieIdWithoutContent(random, 0, TEST_SIZE)).isEmpty();
+
+        log.info("-> findByMovieIdWithoutContent");
     }
 
     @Test
     @DisplayName("특정 리뷰 글을 수정한다.")
     void editReviewInfo() {
+        log.info("<- editReviewInfo");
+
+        // DB 에 글 저장
         ReviewEntity testReview = reviewRepo.saveNewReview(testMovie.getMovieId(),
                 testUser.getUserId(), genReview(null, null, 0));
 
@@ -251,34 +316,46 @@ class ReviewRepositoryTest {
                 .userId(UUID.randomUUID())
                 .build();
 
-        log.info(testReview.toString());
-        log.info(replacement.toString());
+        log.fine(testReview.toString());
+        log.fine(replacement.toString());
 
+        // DB 에 글 수정
         ReviewEntity result = reviewRepo.editReviewInfo(testReview.getReviewId(),
                 replacement);
 
+        log.fine(result.toString());
+
+        // 불변값들 수정 안됬는지, 다른거 수정 됬는지 확인
         assertThat(result).satisfies(
                 r -> assertThat(r.getReviewId()).isEqualTo(testReview.getReviewId()),
                 r -> assertThat(r.getUserId()).isEqualTo(testUser.getUserId()),
                 r -> assertThat(r.getMovieId()).isEqualTo(testMovie.getMovieId()),
                 r -> assertThat(r.getCreatedAt()).isEqualTo(testReview.getCreatedAt()),
                 r -> assertThat(r.getTitle()).isEqualTo(replacement.getTitle()),
-                r -> assertThat(r.getContent()).isEqualTo(replacement.getContent())
+                r -> assertThat(r.getContent()).isEqualTo(replacement.getContent()),
+                r -> assertThat(r.getUpdatedAt()).isNotNull()
         );
 
-        log.info(result.toString());
+        log.info("-> editReviewInfo");
     }
 
     @Test
     @DisplayName("특정 리뷰 글을 삭제한다.")
     void deleteReview() {
+        log.info("<- deleteReview");
+
+        // DB 에 글 저장
         ReviewEntity testReview = reviewRepo.saveNewReview(testMovie.getMovieId(),
                 testUser.getUserId(), genReview(null, null, 0));
 
+        // DB 에 진짜 저장됬는지 확인
         assertThat(reviewRepo.findByReviewId(testReview.getReviewId())).isNotEmpty();
 
         reviewRepo.deleteReview(testReview.getReviewId());
 
+        // 삭제 됬는지 확인
         assertThat(reviewRepo.findByReviewId(testReview.getReviewId())).isEmpty();
+
+        log.info("-> deleteReview");
     }
 }
