00bdb384edafb1cef0cd0c5b4d46d8a68985a3de
hgh1472 <hgh1472@naver.com>
2024-10-23 11:22:23 +0900

    rename: 기존 마이바티스 구현체 위치 및 클래스명 변경
    Related to: #150
    Ref: #136

--- application/src/main/java/core/application/users/repositories/mybatis/MybatisUserRepository.java
+++ application/src/main/java/core/application/users/repositories/mybatis/MybatisUserRepository.java
@@ -1,9 +1,12 @@
-package core.application.users.repositories;
+package core.application.users.repositories.mybatis;
 
 import core.application.users.mapper.UserMapper;
 import core.application.users.models.entities.UserEntity;
 import core.application.users.models.entities.UserRole;
+import core.application.users.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,7 +21,8 @@ import java.util.UUID;
  */
 @Repository
 @RequiredArgsConstructor
-public class UserRepositoryImpl implements UserRepository{
+@Profile("mybatis")
+public class MybatisUserRepository implements UserRepository{
 
     private final UserMapper mapper;
 
