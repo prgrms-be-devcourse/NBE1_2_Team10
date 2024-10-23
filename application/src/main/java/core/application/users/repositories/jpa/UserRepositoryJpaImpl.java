cbbac7e74cc6ebead69e2d269fc7780a0602f0c6
hgh1472 <hgh1472@naver.com>
2024-10-23 11:23:23 +0900

    feat: `UserEntity` JPA 인터페이스 및 UserRepository 구현
    Related to: #150
    Ref: #136

---
+++ application/src/main/java/core/application/users/repositories/jpa/UserRepositoryJpaImpl.java
@@ -0,0 +1,71 @@
+package core.application.users.repositories.jpa;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import core.application.users.models.entities.UserEntity;
+import core.application.users.models.entities.UserRole;
+import core.application.users.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+@Profile("jpa")
+public class UserRepositoryJpaImpl implements UserRepository {
+	private final JpaUserRepository jpaRepository;
+
+	@Override
+	public int saveNewUser(UserEntity newUser) {
+		jpaRepository.save(newUser);
+		return jpaRepository.findById(newUser.getUserId()).isPresent() ? 1 : 0;
+	}
+
+	@Override
+	public Optional<UserEntity> findByUserId(UUID userId) {
+		return jpaRepository.findById(userId);
+	}
+
+	@Override
+	public Boolean existsByEmail(String email) {
+		return jpaRepository.existsByUserEmail(email);
+	}
+
+	@Override
+	public Optional<UserEntity> findByUserEmail(String email) {
+		return jpaRepository.findByUserEmail(email);
+	}
+
+	@Override
+	public Optional<UserEntity> findByUserEmailAndPassword(String email, String password) {
+		return jpaRepository.findByUserEmailAndUserPw(email, password);
+	}
+
+	@Override
+	public List<UserEntity> findByUserRole(UserRole role) {
+		return jpaRepository.findByRole(role);
+	}
+
+	@Override
+	public List<UserEntity> findAll() {
+		return jpaRepository.findAll();
+	}
+
+	@Override
+	public int editUserInfo(UserEntity replacement) {
+		if (jpaRepository.existsById(replacement.getUserId())) {
+			jpaRepository.save(replacement);
+			return 1;
+		}
+		return 0;
+	}
+
+	@Override
+	public int deleteUser(UUID userId) {
+		jpaRepository.deleteById(userId);
+		return jpaRepository.findById(userId).isEmpty() ? 1 : 0;
+	}
+}
