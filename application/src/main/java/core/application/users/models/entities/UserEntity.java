a11af2262d91b0a89b5be6055f04c134f738bc04
hgh1472 <hgh1472@naver.com>
2024-10-23 11:22:50 +0900

    refactor: `UserEntity` JPA 어노테이션 추가
    Related to: #150
    Ref: #136

--- application/src/main/java/core/application/users/models/entities/UserEntity.java
+++ application/src/main/java/core/application/users/models/entities/UserEntity.java
@@ -1,18 +1,38 @@
 package core.application.users.models.entities;
 
-import lombok.*;
-
 import java.util.UUID;
+
+import core.application.config.jpa.UUIDConverter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @ToString
+@Entity(name = "user_table")
 public class UserEntity {
+    @Id
+    @Convert(converter = UUIDConverter.class)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "binary(16)")
     private UUID     userId;
     private String   userEmail;
     private String   userPw;
+    @Enumerated(EnumType.STRING)
     private UserRole role;
     private String   alias;
     private String   phoneNum;
