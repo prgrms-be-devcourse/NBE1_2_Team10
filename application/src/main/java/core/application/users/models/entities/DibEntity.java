4d35fe6681898c030c032ba2a6a0121e79b00cac
jbw9964 <jbw9964@gmail.com>
2024-10-25 17:48:47 +0900

    refactor: `Dib`, `User` 엔티티 `equals`, `hashCode` 메서드 추가
    테스트 시 `isEqualTo` 에 용이하여 수정

--- application/src/main/java/core/application/users/models/entities/DibEntity.java
+++ application/src/main/java/core/application/users/models/entities/DibEntity.java
@@ -1,8 +1,7 @@
 package core.application.users.models.entities;
 
+import java.util.*;
 import lombok.*;
-
-import java.util.UUID;
 
 @Getter
 @Builder
@@ -13,4 +12,26 @@ public class DibEntity {
     private Long   dibId;
     private UUID   userId;
     private String movieId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        DibEntity dibEntity = (DibEntity) o;
+        return Objects.equals(dibId, dibEntity.dibId) && Objects.equals(userId,
+                dibEntity.userId) && Objects.equals(movieId, dibEntity.movieId);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(dibId);
+        result = 31 * result + Objects.hashCode(userId);
+        result = 31 * result + Objects.hashCode(movieId);
+        return result;
+    }
 }
